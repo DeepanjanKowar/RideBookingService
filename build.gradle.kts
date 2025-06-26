01da3ec43e2b298d93fcc12224379af170fb8b37
Deepanjan Kowar <deepanjan.kowar@gmail.com>
2025-06-26 12:34:06 +0530

    Configure serialization plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Use a stable Kotlin version so IDEs can resolve the Gradle plugin
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.22" apply false
+    kotlin("plugin.serialization") version "1.9.22" apply false
 }
 
 allprojects {
